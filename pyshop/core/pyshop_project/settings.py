962f6cfe84a9dbf511e92e6d70095970a8b96350
Raj Tiwari <rajtiwari16916@gmail.com>
2026-05-08 19:02:21 +0000

    auto create superuser

--- pyshop/core/pyshop_project/settings.py
+++ pyshop/core/pyshop_project/settings.py
@@ -128,3 +128,20 @@ RAZORPAY_KEY_ID = "rzp_test_Sla3lKb4vNCgJn"
 RAZORPAY_KEY_SECRET = "MRmvpp8DBC96DO2953XD5Om7"
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
+email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+
+if username and email and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password
+        )
